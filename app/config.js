dba17f5e8953c337c5e0fc4a6ed4bfb7cfed1d29
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-12 18:18:59 +0100

    small refators, restructures, ts definitions

---
+++ app/config.js
@@ -0,0 +1,10 @@
+(() => {
+    'use strict';
+    angular.module('puzzleGame')
+        .config(function ($routeProvider, $locationProvider, $httpProvider) {
+        $routeProvider.otherwise({ redirectTo: '/' });
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    });
+})();
+//# sourceMappingURL=config.js.map
---
+++ app/config.ts
@@ -0,0 +1,12 @@
+
+declare var angular;
+(() => {
+    'use strict';
+    angular.module('puzzleGame')
+        .config(function($routeProvider, $locationProvider, $httpProvider) {
+            $routeProvider.otherwise({ redirectTo: '/' });
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+        });
+})();
