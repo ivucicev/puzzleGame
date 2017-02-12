f336ce336967441f056ddd7141274b339aace197
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-12 20:09:42 +0100

    handling user input

--- app/components/Game/Controllers/GameController.js
+++ app/components/Game/Controllers/GameController.js
@@ -6,10 +6,17 @@
         let self = this;
         this.generatedWord = {};
         this.answer = [];
+        this.currentIndex = 0;
+        this.currentScore = 0;
         this.init = () => {
             this.generatedWord = GameService.scrambleWord();
             this.answer = new Array(this.generatedWord.word.length);
             console.log(this.answer);
+        };
+        this.scrambleCurrentWord = () => {
+        };
+        this.handleUserInput = ($event) => {
+            console.log($event.key);
         };
         this.init();
     }
---
+++ app/components/Game/Directives/Keypress.ts
@@ -0,0 +1,24 @@
+(() => {
+    "use strict";
+    angular.module('puzzleGame.Game')
+    .directive('keypress', [Keypress]);
+    function Keypress () {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: { isolateValue: "@" },
+            terminal: true,
+            link: function postLink(scope, iElement, iAttrs) {
+                let keypresEvent = e => scope.$apply(scope.$parent.Game.handleUserInput(e));
+                try {
+                    document.body.addEventListener('keydown', keypresEvent);​​​​​​​
+                    scope.$on('$destroy', () => {
+                        document.body.removeEventListener('keydown', keypresEvent);​​​​​​​
+                    });
+                } catch (e) {
+                    document.body.removeEventListener('keydown', keypresEvent);​​​​​​​
+                }
+            }
+        }
+    }
+})();
