436579505a973d07440e3217d7d3c8c9b2f08782
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-12 21:19:18 +0100

    cleanup

--- app/app.js
+++ app/app.js
@@ -3,9 +3,6 @@
     angular.module('puzzleGame', [
         'ngRoute',
         "puzzleGame.Game",
-        'mostPopularListingsApp.home',
-        'mostPopularListingsApp.about',
-        'mostPopularListingsApp.login'
     ]);
 })();
 //# sourceMappingURL=app.js.map
--- app/app.ts
+++ app/app.ts
@@ -3,8 +3,5 @@
     angular.module('puzzleGame', [
         'ngRoute',
         "puzzleGame.Game",
-        'mostPopularListingsApp.home',
-        'mostPopularListingsApp.about',
-        'mostPopularListingsApp.login'
     ]);
 })();
