17dbfd3adae88413ca8a43c9aba66e10e1d90984
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-12 20:48:35 +0100

    next word

--- app/components/Game/Controllers/GameController.js
+++ app/components/Game/Controllers/GameController.js
@@ -10,11 +10,18 @@
         this.score = 0;
         this.currentScoreDecrease = 0;
         this.init = () => {
+            this.nextWord();
+        };
+        this.nextWord = () => {
+            this.currentScoreDecrease = 0;
+            this.answer = [];
+            this.currentIndex = 0;
             this.generatedWord = GameService.scrambleWord();
             this.answer = new Array(this.generatedWord.word.length);
         };
         this.validateAnswer = () => {
             if (this.generatedWord.word === this.answer.join('')) {
+                this.nextWord();
                 this.increaseScore();
             }
         };
@@ -38,6 +45,7 @@
                 this.currentIndex--;
             }
         };
+        // triggered from directive, which listens for keypress
         this.handleUserInput = ($event) => {
             if (this.keyValid($event.keyCode)) {
                 this.answer[this.currentIndex] = $event.key.toUpperCase();
