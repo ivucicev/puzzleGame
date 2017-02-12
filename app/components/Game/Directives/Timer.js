41f9dd7e73bffe2549b6521755a80fbce72472e8
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-12 21:11:09 +0100

    40 sec game timer

---
+++ app/components/Game/Directives/Timer.js
@@ -0,0 +1,31 @@
+(() => {
+    "use strict";
+    angular.module('puzzleGame.Game')
+        .directive('timer', ['$interval', Timer]);
+    function Timer($interval) {
+        return {
+            restrict: 'E',
+            scope: {
+                date: '@'
+            },
+            link: (scope, element) => {
+                let intvl = null;
+                try {
+                    let timer = 40;
+                    intvl = $interval(() => {
+                        element.text(--timer);
+                        if (timer == 0) {
+                            scope.$apply(scope.$parent.Game.timesUp());
+                            $interval.cancel(intvl);
+                        }
+                    }, 1000);
+                    scope.$on('$destroy', () => $interval.cancel(intvl));
+                }
+                catch (e) {
+                    $interval.cancel(intvl);
+                }
+            }
+        };
+    }
+})();
+//# sourceMappingURL=Timer.js.map
